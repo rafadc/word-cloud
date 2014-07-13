9c1ca0791ca5a9a787c7999e4a254dc41e7ce58e
Rafa de Castro <rafael@micubiculo.com>
2014-07-14 01:05:04 +0200

    CSS file was missing. Also format of input was incorrect

--- generate.rb
+++ generate.rb
@@ -1,12 +1,12 @@
 require 'json'
 require_relative './renderer'
 
-words = {
-  lorem: 15,
-  ipsum: 6,
-  dolor: 23,
-  sit: 2,
-  amet: 45
-}
+words = [
+         {text: 'lorem', weight: 15},
+         {text: 'ipsum', weight: 6},
+         {text: 'dolor', weight: 36},
+         {text: 'sit', weight: 4},
+         {text: 'amet', weight: 12}
+]
 
-puts render('template.html.erb', {words: words})
+render('template.html.erb', {words: words})
